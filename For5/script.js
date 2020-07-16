8dbe811280f3f0a310be4334929722297f785153
Pavels Jakimovics <karina@MacBook-Pro-Pavels.local>
2020-07-16 16:57:12 +0300

    For5 correct

--- For5/script.js
+++ For5/script.js
@@ -1,12 +1,11 @@
 "use strict";
 
-const {getRandomIntInterval, getRandomFloatInterval} = require('./utils') // Импортируем функции из файла utils.js
-
 // Дано вещественное число — цена 1 кг конфет. Вывести стоимость 0.1, 0.2, ..., 1 кг конфет.
 
 let count = 0; 
 
-for( let A = 1 ; A <= 10; A++){
+
+for( let A = 0.1 ; A <= 1; A = A + 0.1){
   count++
-  console.log('Цена за ' + A/10 + ' кг' + ' ровна ' + count + ' руб' )
+  console.log('Цена за ' + A + ' кг' + ' ровна ' + count + ' руб' )
 }
