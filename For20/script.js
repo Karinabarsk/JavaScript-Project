96e212114a3a6d595f3fb6179fc6927ac08ec607
Pavels Jakimovics <karina@MacBook-Pro-Pavels.local>
2020-07-18 17:21:12 +0300

    For20

---
+++ For20/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const {getRandomIntInterval} = require('../utils') // Импортируем функции из файла utils.js
+
+// Дано целое число N (> 0). Используя один цикл, найти сумму "1! + 2! + 3! + ... + N!"
+
+const N = getRandomIntInterval(1, 20)
+let sum = 0
+let F = 1
+
+for(let i = 1; i<=N ; i++){
+    F *= i
+    sum += F 
+}
+console.log(sum)
